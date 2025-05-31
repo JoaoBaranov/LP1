8cb1ad7c4c284faec3e30dbadbdc7b98424fc0af
Joao Vitor Araujo Baranov <baranovjoao@outlook.com>
2025-05-30 21:00:28 -0300

    Save

---
+++ LP/src/main/java/Controller/AnimalController.java
@@ -0,0 +1,45 @@
+package Controller;
+import javafx.fxml.FXML;
+
+public class AnimalController {
+
+
+    public Animal(String nome, String especie, int idade, String porte) {
+        this.nome = nome;
+        this.especie = especie;
+        this.idade = idade;
+        this.porte = porte;
+    }
+
+    public String mostrarDados() {
+        return "Nome: " + nome +
+                "\nEspécie: " + especie +
+                "\nIdade: " + idade + " anos" +
+                "\nPorte: " + porte;
+    }
+
+    public String classificarIdade() {
+        if (idade <= 1) {
+            return nome + " é um filhote.";
+        } else if (idade <= 7) {
+            return nome + " é um animal adulto.";
+        } else {
+            return nome + " é um animal idoso.";
+        }
+    }
+
+    public String emitirSom() {
+        switch (especie.toLowerCase()) {
+            case "cachorro":
+                return nome + " diz: Au Au!";
+            case "gato":
+                return nome + " diz: Miau!";
+            case "pássaro":
+                return nome + " diz: Piu piu!";
+            case "vaca":
+                return nome + " diz: Muuuu!";
+            default:
+                return nome + " emite um som desconhecido.";
+        }
+    }
+}
