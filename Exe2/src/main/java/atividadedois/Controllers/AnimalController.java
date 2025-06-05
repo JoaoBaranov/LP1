38e902a97e67966d950e1c6368bae022b5192e63
Joao Vitor Araujo Baranov <baranovjoao@outlook.com>
2025-06-04 23:03:38 -0300

    Save

--- Exe2/src/main/java/atividadedois/Controllers/AnimalController.java
+++ Exe2/src/main/java/atividadedois/Controllers/AnimalController.java
@@ -1,19 +1,63 @@
 package atividadedois.Controllers;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.event.ActionEvent;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 
 public class AnimalController {
 
+    @FXML
+    private TextField nomeTextField;
+
+    @FXML
+    private TextField especieTextField;
+
+    @FXML
+    private TextField idadeTextField;
+
+    @FXML
+    private TableView<?> tabelaAnimais;
+
+    @FXML
+    private TableColumn<?, ?> colunaNome;
+
+    @FXML
+    private TableColumn<?, ?> colunaEspecie;
+
+    @FXML
+    private TableColumn<?, ?> colunaIdade;
+
+    @FXML
+    private ImageView imageView;
 
     @FXML
     private Button botaoMenu;
+
+    @FXML
+    private Button botaoAdicionar;
+
+    @FXML
+    private Button botaoEditar;
+
+    @FXML
+    private Button botaoExcluir;
+
+    @FXML
+    private Button botaoCarinho;
+
+    @FXML
+    private Button botaoBrincar;
+
+    @FXML
+    private Button botaoAlimentar;
 
 
     @FXML
@@ -26,5 +70,107 @@ public class AnimalController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    private void carinhoOn(ActionEvent event) {
+        String nome = nomeTextField.getText();
+        String especie = especieTextField.getText();
+        String idade = idadeTextField.getText();
+        if (nome == null || nome.trim().isEmpty()) {
+            Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+            alertaErro.setTitle("Erro");
+            alertaErro.setHeaderText(null);
+            alertaErro.setContentText("Informe o nome do animal.");
+            alertaErro.showAndWait();
+        } else if(especie == null || especie.trim().isEmpty()) {
+            Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+            alertaErro.setTitle("Erro");
+            alertaErro.setHeaderText(null);
+            alertaErro.setContentText("Informe a especie do animal.");
+            alertaErro.showAndWait();
+        } else if(idade == null || idade.trim().isEmpty()) {
+            Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+            alertaErro.setTitle("Erro");
+            alertaErro.setHeaderText(null);
+            alertaErro.setContentText("Informe a idade do animal.");
+            alertaErro.showAndWait();
+        } else {
+            String mensagem = nome + " recebeu carinho, que fofura!";
+            Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+            alerta.setTitle("Ação");
+            alerta.setHeaderText(null);
+            alerta.setContentText(mensagem);
+            alerta.showAndWait();
+        }
+
+    }
+
+    @FXML
+    private void brincarOn(ActionEvent event) {
+        String nome = nomeTextField.getText();
+        String especie = especieTextField.getText();
+        String idade = idadeTextField.getText();
+        if (nome == null || nome.trim().isEmpty()) {
+            Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+            alertaErro.setTitle("Erro");
+            alertaErro.setHeaderText(null);
+            alertaErro.setContentText("Informe o nome do animal.");
+            alertaErro.showAndWait();
+        } else if(especie == null || especie.trim().isEmpty()) {
+            Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+            alertaErro.setTitle("Erro");
+            alertaErro.setHeaderText(null);
+            alertaErro.setContentText("Informe a especie do animal.");
+            alertaErro.showAndWait();
+        } else if(idade == null || idade.trim().isEmpty()) {
+            Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+            alertaErro.setTitle("Erro");
+            alertaErro.setHeaderText(null);
+            alertaErro.setContentText("Informe a idade do animal.");
+            alertaErro.showAndWait();
+        } else {
+            String mensagem = nome + " recebeu carinho, que fofura!";
+            Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+            alerta.setTitle("Ação");
+            alerta.setHeaderText(null);
+            alerta.setContentText(mensagem);
+            alerta.showAndWait();
+        }
+
+    }
+
+    @FXML
+    private void alimentarOn(ActionEvent event) {
+        String nome = nomeTextField.getText();
+        String especie = especieTextField.getText();
+        String idade = idadeTextField.getText();
+        if (nome == null || nome.trim().isEmpty()) {
+            Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+            alertaErro.setTitle("Erro");
+            alertaErro.setHeaderText(null);
+            alertaErro.setContentText("Informe o nome do animal.");
+            alertaErro.showAndWait();
+        } else if(especie == null || especie.trim().isEmpty()) {
+            Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+            alertaErro.setTitle("Erro");
+            alertaErro.setHeaderText(null);
+            alertaErro.setContentText("Informe a especie do animal.");
+            alertaErro.showAndWait();
+        } else if(idade == null || idade.trim().isEmpty()) {
+            Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+            alertaErro.setTitle("Erro");
+            alertaErro.setHeaderText(null);
+            alertaErro.setContentText("Informe a idade do animal.");
+            alertaErro.showAndWait();
+        } else {
+            String mensagem = nome + " recebeu carinho, que fofura!";
+            Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+            alerta.setTitle("Ação");
+            alerta.setHeaderText(null);
+            alerta.setContentText(mensagem);
+            alerta.showAndWait();
+        }
+
     }
 }
--- Exe2/src/main/java/atividadedois/Controllers/CalcadoController.java
+++ Exe2/src/main/java/atividadedois/Controllers/CalcadoController.java
@@ -7,15 +7,51 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import javafx.scene.control.Alert.AlertType;
 
 
 public class CalcadoController {
 
+    @FXML
+    private TextField marcaTextField;
+
+    @FXML
+    private TextField modeloTextField;
+
+    @FXML
+    private TextField tamanhoTextField;
+
+    @FXML
+    private TableView<?> tabelaCalcados;
+
+    @FXML
+    private TableColumn<?, ?> colunaMarca;
+
+    @FXML
+    private TableColumn<?, ?> colunaModelo;
+
+    @FXML
+    private TableColumn<?, ?> colunaTamanho;
+
+    @FXML
+    private Button botaoAdicionar;
+
+    @FXML
+    private Button botaoEditar;
+
+    @FXML
+    private Button botaoExcluir;
+
+    @FXML
+    private Button botaoBotar;
+
+    @FXML
+    private Button botaoTestar;
+
+    @FXML
+    private Button botaoEspelho;
 
     @FXML
     private Button botaoMenu;
@@ -32,4 +68,109 @@ public class CalcadoController {
             e.printStackTrace();
         }
     }
-}
+
+    @FXML
+    private void botarOn(ActionEvent event) {
+        String marca = marcaTextField.getText();
+        String modelo = modeloTextField.getText();
+        String tamanho = tamanhoTextField.getText();
+        if (marca == null || marca.trim().isEmpty()) {
+            Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+            alertaErro.setTitle("Erro");
+            alertaErro.setHeaderText(null);
+            alertaErro.setContentText("Informe a marca do calçado.");
+            alertaErro.showAndWait();
+        } else if(modelo == null || modelo.trim().isEmpty()) {
+            Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+            alertaErro.setTitle("Erro");
+            alertaErro.setHeaderText(null);
+            alertaErro.setContentText("Informe o modelo do calçado.");
+            alertaErro.showAndWait();
+        } else if(tamanho == null || tamanho.trim().isEmpty()) {
+            Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+            alertaErro.setTitle("Erro");
+            alertaErro.setHeaderText(null);
+            alertaErro.setContentText("Informe o tamanho do calçado.");
+            alertaErro.showAndWait();
+        } else {
+            String mensagem = " Você colocou o(a) " + modelo;
+            Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+            alerta.setTitle("Ação");
+            alerta.setHeaderText(null);
+            alerta.setContentText(mensagem);
+            alerta.showAndWait();
+        }
+
+    }
+
+    @FXML
+    private void testarOn(ActionEvent event) {
+        String marca = marcaTextField.getText();
+        String modelo = modeloTextField.getText();
+        String tamanho = tamanhoTextField.getText();
+        if (marca == null || marca.trim().isEmpty()) {
+            Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+            alertaErro.setTitle("Erro");
+            alertaErro.setHeaderText(null);
+            alertaErro.setContentText("Informe a marca do calçado.");
+            alertaErro.showAndWait();
+        } else if(modelo == null || modelo.trim().isEmpty()) {
+            Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+            alertaErro.setTitle("Erro");
+            alertaErro.setHeaderText(null);
+            alertaErro.setContentText("Informe o modelo do calçado.");
+            alertaErro.showAndWait();
+        } else if(tamanho == null || tamanho.trim().isEmpty()) {
+            Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+            alertaErro.setTitle("Erro");
+            alertaErro.setHeaderText(null);
+            alertaErro.setContentText("Informe o tamanho do calçado.");
+            alertaErro.showAndWait();
+        } else {
+            String mensagem = " É FUGAAAA, você correu da loja com o(a) " + modelo;
+            Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+            alerta.setTitle("Ação");
+            alerta.setHeaderText(null);
+            alerta.setContentText(mensagem);
+            alerta.showAndWait();
+        }
+
+    }
+    @FXML
+    private void espelhoOn(ActionEvent event) {
+        String marca = marcaTextField.getText();
+        String modelo = modeloTextField.getText();
+        String tamanho = tamanhoTextField.getText();
+        if (marca == null || marca.trim().isEmpty()) {
+            Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+            alertaErro.setTitle("Erro");
+            alertaErro.setHeaderText(null);
+            alertaErro.setContentText("Informe a marca do calçado.");
+            alertaErro.showAndWait();
+        } else if(modelo == null || modelo.trim().isEmpty()) {
+            Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+            alertaErro.setTitle("Erro");
+            alertaErro.setHeaderText(null);
+            alertaErro.setContentText("Informe o modelo do calçado.");
+            alertaErro.showAndWait();
+        } else if(tamanho == null || tamanho.trim().isEmpty()) {
+            Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+            alertaErro.setTitle("Erro");
+            alertaErro.setHeaderText(null);
+            alertaErro.setContentText("Informe o tamanho do calçado.");
+            alertaErro.showAndWait();
+        } else {
+            String mensagem = " Você experimentou o(a) " + modelo + ", agora está na moda!";
+            Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+            alerta.setTitle("Ação");
+            alerta.setHeaderText(null);
+            alerta.setContentText(mensagem);
+            alerta.showAndWait();
+        }
+
+    }
+
+    }
+
+
+
